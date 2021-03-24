831797667c608d3e85611396232157019f460126
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2021-03-24 15:01:29 -0700

    Clean up and simplify renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,14 +1,16 @@
 export { render };
 
+import { CompiledAttribute, CompiledValue, Compiler } from "./compiler";
 import { Template } from "./template";
-import {
-  CompiledAttribute,
-  CompiledTemplate,
-  CompiledValue,
-} from "./compiled-template";
 
-const compiledCache = new WeakMap<Comment, CompiledTemplate>();
-const templateCache = new WeakMap<Comment, Template>();
+interface RenderCache {
+  attributes: Record<number, CompiledAttribute>;
+  values: Record<number, CompiledValue>;
+  template: Template;
+}
+
+const rendered = new WeakSet<Node>();
+const caches = new WeakMap<Comment, RenderCache>();
 
 function renderAttribute(
   { kind, element, name }: CompiledAttribute,
@@ -35,57 +37,80 @@ function renderValues(
   newValue: unknown
 ): void {
   if (kind === "template") {
-    renderRange(start, end, newValue as Template);
-  } else if (kind === "text") {
-    if (start.nextSibling === null || start.nextSibling === end) {
-      start.after(new Text(String(newValue)));
+    if (newValue instanceof Template) {
+      renderTemplate(start, end, newValue);
     } else {
-      start.nextSibling.nodeValue = String(newValue);
+      throw new Error("value must remain as a template");
+    }
+  } else if (kind === "text") {
+    const value = String(newValue);
+
+    if (start.nextSibling === null || start.nextSibling === end) {
+      start.after(new Text(value));
+    } else {
+      start.nextSibling.nodeValue = value;
     }
   }
 }
 
-function renderRange(start: Comment, end: Comment, template: Template): void {
-  let compiled = compiledCache.get(start);
+function renderTemplate(
+  start: Comment,
+  end: Comment,
+  template: Template
+): void {
+  let rendered = true;
+  let cache = caches.get(start);
 
-  if (typeof compiled === "undefined") {
-    compiled = new CompiledTemplate(template);
-    start.after(compiled.fragment);
+  if (typeof cache === "undefined") {
+    const compiler = new Compiler(template);
+
+    rendered = false;
+    cache = {
+      attributes: compiler.attributes,
+      values: compiler.values,
+      template: compiler.template,
+    };
+
+    start.after(compiler.fragment);
   }
 
-  const oldValues = templateCache.get(start)?.values ?? [];
+  const oldValues = cache.values;
   const newValues = template.values;
 
   for (let index = 0; index < newValues.length; index++) {
     const oldValue = oldValues[index];
     const newValue = newValues[index];
 
-    if (oldValues.length === 0 || oldValue !== newValue) {
-      if (index in compiled.attributes) {
-        renderAttribute(compiled.attributes[index], oldValue, newValue);
-      } else if (index in compiled.values) {
-        renderValues(compiled.values[index], oldValue, newValue);
-      }
+    if (oldValue === newValue && rendered) {
+      continue;
+    }
+
+    if (index in cache.attributes) {
+      renderAttribute(cache.attributes[index], oldValue, newValue);
+    } else if (index in cache.values) {
+      renderValues(cache.values[index], oldValue, newValue);
+    } else {
+      throw new Error("index does not exist in template");
     }
   }
-
-  compiledCache.set(start, compiled);
-  templateCache.set(start, template);
 }
 
 function render(target: Element, template: Template): void {
-  if (target.childNodes.length === 0) {
+  if (!rendered.has(target)) {
     while (target.firstChild) {
       target.firstChild.remove();
     }
 
     target.append(new Comment(), new Comment());
+    rendered.add(target);
   }
 
   const start = target.firstChild;
   const end = target.lastChild;
 
   if (start instanceof Comment && end instanceof Comment) {
-    renderRange(start, end, template);
+    renderTemplate(start, end, template);
+  } else {
+    throw new Error("start and end markers are missing");
   }
 }
