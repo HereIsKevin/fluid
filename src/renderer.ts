777ad6272d28d7c5f5c379c99d56e8dc2b53e2d0
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2021-03-23 19:26:04 -0700

    Simplify and improve renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,178 +1,91 @@
-export { RendererAttribute, RendererValue, Renderer };
+export { render };
 
 import { Template } from "./template";
+import {
+  CompiledAttribute,
+  CompiledTemplate,
+  CompiledValue,
+} from "./compiled-template";
 
-interface RendererAttribute {
-  element: Element;
-  name: string;
-}
+const compiledCache = new WeakMap<Comment, CompiledTemplate>();
+const templateCache = new WeakMap<Comment, Template>();
 
-interface RendererValue {
-  start: Comment;
-  end: Comment;
-}
-
-class Renderer {
-  protected static instances: WeakMap<Comment, Renderer> = new WeakMap();
-  protected static values: WeakMap<Comment, unknown[]> = new WeakMap();
-
-  protected attributes: Record<number, RendererAttribute>;
-  protected values: Record<number, RendererValue>;
-
-  protected fragment?: DocumentFragment;
-
-  public constructor(template: Template) {
-    this.attributes = {};
-    this.values = {};
-
-    this.fragment = template.generate();
-    this.compile(this.fragment);
-  }
-
-  private compile(node: Node): void {
-    if (node instanceof Element) {
-      this.compileAttributes(node);
+function renderAttribute(
+  { kind, element, name }: CompiledAttribute,
+  oldValue: unknown,
+  newValue: unknown
+): void {
+  if (kind === "event") {
+    element.removeEventListener(name, oldValue as EventListener);
+    element.addEventListener(name, newValue as EventListener);
+  } else if (kind === "toggle") {
+    if (Boolean(newValue)) {
+      element.setAttribute(name, "");
+    } else {
+      element.removeAttribute(name);
     }
-
-    this.compileValues(node);
-
-    for (const child of node.childNodes) {
-      this.compile(child);
-    }
-  }
-
-  private compileAttributes(element: Element): void {
-    for (const name of element.getAttributeNames()) {
-      const value = element.getAttribute(name) ?? "";
-      const matches = value.match(/^<!--([0-9]+)-->$/);
-
-      if (matches !== null) {
-        const index = Number(matches[1]);
-        const attribute = { element, name };
-
-        this.attributes[index] = attribute;
-      }
-    }
-  }
-
-  private compileValues(node: Node): void {
-    let current = node.firstChild;
-
-    while (current !== null) {
-      if (current instanceof Comment) {
-        const value = current.nodeValue ?? "";
-        const matches = value.match(/^([0-9]+)$/);
-
-        if (matches !== null) {
-          const start = new Comment();
-          const end = new Comment();
-
-          current.replaceWith(start, end);
-          current = end;
-
-          const index = Number(matches[1]);
-          const value = { start, end };
-
-          this.values[index] = value;
-        }
-      }
-
-      current = current.nextSibling;
-    }
-  }
-
-  private clearNodes(start: Comment, end: Comment) {
-    let current = start.nextSibling;
-
-    while (current !== null && current !== end) {
-      current.remove();
-      current = start.nextSibling;
-    }
-  }
-
-  private updateAttribute(
-    { element, name }: RendererAttribute,
-    oldValue: unknown,
-    newValue: unknown
-  ): void {
+  } else if (kind === "value") {
     element.setAttribute(name, String(newValue));
   }
+}
 
-  private updateValue(
-    { start, end }: RendererValue,
-    oldValue: unknown,
-    newValue: unknown
-  ): void {
-    if (typeof newValue === "string") {
-      if (typeof oldValue !== "string") {
-        this.clearNodes(start, end);
-      }
-
-      if (start.nextSibling !== null && start.nextSibling !== end) {
-        start.nextSibling.nodeValue = newValue;
-      } else {
-        start.after(new Text(newValue));
-      }
-    } else if (newValue instanceof Template) {
-      if (!(oldValue instanceof Template)) {
-        this.clearNodes(start, end);
-      }
-
-      let instance = Renderer.instances.get(start);
-
-      if (typeof instance === "undefined") {
-        instance = new Renderer(newValue);
-      }
-
-      instance.renderRange(start, end, newValue.values);
+function renderValues(
+  { kind, start, end }: CompiledValue,
+  oldValue: unknown,
+  newValue: unknown
+): void {
+  if (kind === "template") {
+    renderRange(start, end, newValue as Template);
+  } else if (kind === "text") {
+    if (start.nextSibling === null || start.nextSibling === end) {
+      start.after(new Text(String(newValue)));
+    } else {
+      start.nextSibling.nodeValue = String(newValue);
     }
   }
+}
 
-  public update(index: number, oldValue: unknown, newValue: unknown): void {
-    if (index in this.attributes) {
-      this.updateAttribute(this.attributes[index], oldValue, newValue);
-    } else if (index in this.values) {
-      this.updateValue(this.values[index], oldValue, newValue);
-    }
+function renderRange(start: Comment, end: Comment, template: Template): void {
+  let compiled = compiledCache.get(start);
+
+  if (typeof compiled === "undefined") {
+    compiled = new CompiledTemplate(template);
+    start.after(compiled.fragment);
   }
 
-  private renderRange(start: Comment, end: Comment, newValues: unknown[]) {
-    const oldValues = Renderer.values.get(start) ?? [];
+  const oldValues = templateCache.get(start)?.values ?? [];
+  const newValues = template.values;
 
-    Renderer.values.set(start, newValues);
-    Renderer.instances.set(start, this);
+  for (let index = 0; index < newValues.length; index++) {
+    const oldValue = oldValues[index];
+    const newValue = newValues[index];
 
-    if (typeof this.fragment !== "undefined") {
-      start.after(this.fragment);
-      this.fragment = undefined;
-    }
-
-    for (let index = 0; index < newValues.length; index++) {
-      const oldValue = oldValues[index];
-      const newValue = newValues[index];
-
-      if (oldValue !== newValue) {
-        this.update(index, oldValue, newValue);
+    if (oldValues.length === 0 || oldValue !== newValue) {
+      if (index in compiled.attributes) {
+        renderAttribute(compiled.attributes[index], oldValue, newValue);
+      } else if (index in compiled.values) {
+        renderValues(compiled.values[index], oldValue, newValue);
       }
     }
   }
 
-  public render(target: Element, values: unknown[]): void {
-    if (!(target.firstChild instanceof Comment)) {
-      target.append(new Comment());
+  compiledCache.set(start, compiled);
+  templateCache.set(start, template);
+}
+
+function render(target: Element, template: Template): void {
+  if (target.childNodes.length === 0) {
+    while (target.firstChild) {
+      target.firstChild.remove();
     }
 
-    if (
-      target.lastChild === target.firstChild ||
-      !(target.lastChild instanceof Comment)
-    ) {
-      target.append(new Comment());
-    }
+    target.append(new Comment(), new Comment());
+  }
 
-    this.renderRange(
-      target.firstChild as Comment,
-      target.lastChild as Comment,
-      values
-    );
+  const start = target.firstChild;
+  const end = target.lastChild;
+
+  if (start instanceof Comment && end instanceof Comment) {
+    renderRange(start, end, template);
   }
 }
