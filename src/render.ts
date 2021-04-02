fe03b1f9a0c3c079a8c42ef1695bbada110b1694
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2021-04-02 11:15:59 -0700

    New rendering system

--- src/render.ts
+++ src/render.ts
@@ -6,22 +6,218 @@ import { Template } from "./template";
 interface RenderCache {
   attributes: Record<number, CompiledAttribute>;
   values: Record<number, CompiledValue>;
-  template: Template;
 }
 
-const rendered = new WeakSet<Node>();
+const templates = new WeakMap<Element, Template>();
 const caches = new WeakMap<Comment, RenderCache>();
+const sequences = new WeakMap<Comment, Comment[]>();
+
+function clearElement(element: Element): void {
+  while (element.firstChild) {
+    element.firstChild.remove();
+  }
+}
+
+function clearNodes(start: Node, end: Node): void {
+  let current = start.nextSibling;
+
+  while (current !== null && current !== end) {
+    current.remove();
+    current = start.nextSibling;
+  }
+}
+
+function renderSequence(
+  start: Comment,
+  end: Comment,
+  oldTemplates: Template[] | undefined,
+  newTemplates: Template[]
+): void {
+  if (typeof oldTemplates === "undefined" || oldTemplates.length === 0) {
+    const separators = [];
+
+    for (const template of newTemplates) {
+      const separator = new Comment();
+      const startMarker = new Comment();
+      const endMarker = new Comment();
+
+      end.before(separator, startMarker, endMarker);
+      renderTemplate(startMarker, endMarker, undefined, template);
+
+      separators.push(separator);
+    }
+
+    sequences.set(start, separators);
+
+    return;
+  }
+
+  if (newTemplates.length === 0) {
+    let current = start.nextSibling;
+
+    while (current !== null && current !== end) {
+      current.remove();
+      current = start.nextSibling;
+    }
+
+    sequences.set(start, []);
+
+    return;
+  }
+
+  oldTemplates = [...oldTemplates];
+
+  const sequence = sequences.get(start);
+  const length = Math.max(newTemplates.length, oldTemplates.length);
+
+  if (typeof sequence === "undefined") {
+    throw new Error("render sequence is missing");
+  }
+
+  let modifier = 0;
+
+  for (let index = 0; index < length; index++) {
+    const position = index - modifier;
+    const oldTemplate = oldTemplates[position];
+
+    if (
+      typeof oldTemplate !== "undefined" &&
+      oldTemplate.equalStrings(newTemplates[position]) &&
+      oldTemplate.equalStrings(newTemplates[index])
+    ) {
+      oldTemplates.splice(position, 1);
+
+      clearNodes(sequence[position], sequence[position + 1] ?? end);
+      sequence[position].remove();
+      sequence.splice(position, 1);
+
+      modifier++;
+    }
+  }
+
+  let index = 0;
+
+  while (index < newTemplates.length) {
+    const oldTemplate = oldTemplates[index];
+    const newTemplate = newTemplates[index];
+
+    if (
+      typeof oldTemplate === "undefined" ||
+      !newTemplate.equalStrings(oldTemplate)
+    ) {
+      const insertion = sequence[index] ?? end;
+      const separator = new Comment();
+      const startMarker = new Comment();
+      const endMarker = new Comment();
+
+      insertion.before(separator, startMarker, endMarker);
+      renderTemplate(startMarker, endMarker, undefined, newTemplate);
+
+      sequence.splice(index, 0, separator);
+    } else {
+      const startMarker = sequence[index].nextSibling;
+      const endMarker = (sequence[index + 1] ?? end).previousSibling;
+
+      if (startMarker instanceof Comment && endMarker instanceof Comment) {
+        renderTemplate(startMarker, endMarker, oldTemplate, newTemplate);
+      } else {
+        throw new Error("start or end markers are missing");
+      }
+    }
+
+    index++;
+  }
+
+  sequences.set(start, sequence);
+}
+
+function renderTemplate(
+  start: Comment,
+  end: Comment,
+  oldTemplate: Template | undefined,
+  newTemplate: Template
+): void {
+  if (
+    typeof oldTemplate === "undefined" ||
+    !oldTemplate.equalStrings(newTemplate)
+  ) {
+    clearNodes(start, end);
+
+    const compiler = new Compiler(newTemplate);
+    const cache = {
+      attributes: compiler.attributes,
+      values: compiler.values,
+    };
+
+    start.after(compiler.fragment);
+
+    for (let index = 0; index < newTemplate.values.length; index++) {
+      const value = newTemplate.values[index];
+
+      if (index in cache.attributes) {
+        renderAttribute(cache.attributes[index], undefined, value);
+      } else if (index in cache.values) {
+        renderValue(cache.values[index], undefined, value);
+      }
+    }
+
+    caches.set(start, cache);
+
+    return;
+  }
+
+  const cache = caches.get(start);
+
+  if (typeof cache === "undefined") {
+    throw new Error("render cache is missing");
+  }
+
+  for (let index = 0; index < newTemplate.values.length; index++) {
+    const oldValue = oldTemplate.values[index];
+    const newValue = newTemplate.values[index];
+
+    if (oldValue !== newValue) {
+      if (index in cache.attributes) {
+        renderAttribute(cache.attributes[index], oldValue, newValue);
+      } else if (index in cache.values) {
+        renderValue(cache.values[index], oldValue, newValue);
+      }
+    }
+  }
+}
+
+function renderText(start: Comment, end: Comment, value: string): void {
+  clearNodes(start, end);
+  start.after(new Text(value));
+}
+
+function renderValue(
+  { kind, start, end }: CompiledValue,
+  oldValue: unknown,
+  newValue: unknown
+): void {
+  if (kind === "sequence") {
+    renderSequence(start, end, oldValue as Template[], newValue as Template[]);
+  } else if (kind === "template") {
+    renderTemplate(start, end, oldValue as Template, newValue as Template);
+  } else if (kind === "text") {
+    renderText(start, end, String(newValue));
+  }
+}
 
 function renderAttribute(
-  { kind, element, name }: CompiledAttribute,
+  { kind, name, element }: CompiledAttribute,
   oldValue: unknown,
   newValue: unknown
 ): void {
   if (kind === "event") {
-    element.removeEventListener(name, oldValue as EventListener);
+    if (typeof oldValue !== "undefined") {
+      element.removeEventListener(name, oldValue as EventListener);
+    }
+
     element.addEventListener(name, newValue as EventListener);
   } else if (kind === "toggle") {
-    if (Boolean(newValue)) {
+    if (newValue) {
       element.setAttribute(name, "");
     } else {
       element.removeAttribute(name);
@@ -31,183 +227,20 @@ function renderAttribute(
   }
 }
 
-function renderSequence(
-  start: Comment,
-  end: Comment,
-  oldTemplates: Template[],
-  newTemplates: Template[]
-): void {
-  if (start.nextSibling === end) {
-    for (const template of newTemplates) {
-      const separator = new Comment("separator");
-
-      end.before(separator);
-      renderTemplate(separator, end, template);
-    }
-
-    return;
-  }
-
-  const separators: Comment[] = [];
-
-  let current = start.nextSibling;
-
-  while (current !== null && current !== end) {
-    if (current instanceof Comment && current.nodeValue === "separator") {
-      separators.push(current);
-    }
-
-    current = current.nextSibling;
-  }
-
-  let length = newTemplates.length;
-
-  while (length > oldTemplates.length) {
-    const separator = separators[separators.length - 1];
-
-    while (end.previousSibling !== null && end.previousSibling !== separator) {
-      end.previousSibling.remove();
-    }
-
-    separator.remove();
-    separators.pop();
-    length--;
-  }
-
-  while (length < newTemplates.length) {
-    const separator = new Comment("separator");
-
-    end.before(separator);
-    renderTemplate(separator, end, newTemplates[length - 1]);
-
-    separators.push(separator);
-    length++;
-  }
-
-  for (let index = 0; index < separators.length; index++) {
-    const startSeparator = separators[index];
-    const endSeparator = separators[index + 1] ?? end;
-    const template = newTemplates[index];
-
-    renderTemplate(startSeparator, endSeparator, template);
-  }
-}
-
-function renderValues(
-  { kind, start, end }: CompiledValue,
-  oldValue: unknown,
-  newValue: unknown
-): void {
-  if (kind === "sequence") {
-    renderSequence(start, end, oldValue as Template[], newValue as Template[]);
-  } else if (kind === "template") {
-    if (newValue instanceof Template) {
-      renderTemplate(start, end, newValue);
-    } else {
-      throw new Error("value must remain as a template");
-    }
-  } else if (kind === "text") {
-    const value = String(newValue);
-
-    if (start.nextSibling === null || start.nextSibling === end) {
-      start.after(new Text(value));
-    } else {
-      start.nextSibling.nodeValue = value;
-    }
-  }
-}
-
-function arrayEqual(
-  firstArray: ArrayLike<unknown>,
-  secondArray: ArrayLike<unknown>
-): boolean {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-
-  for (let index = 0; index < firstArray.length; index++) {
-    const firstValue = firstArray[index];
-    const secondValue = secondArray[index];
-
-    if (firstValue !== secondValue) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function renderTemplate(
-  start: Comment,
-  end: Comment,
-  template: Template
-): void {
-  let rendered = true;
-  let cache = caches.get(start);
-
-  if (
-    typeof cache === "undefined" ||
-    !arrayEqual(cache.template.strings, template.strings)
-  ) {
-    const compiler = new Compiler(template);
-
-    let current = start.nextSibling;
-
-    while (current !== null && current !== end) {
-      current.remove();
-      current = start.nextSibling;
-    }
-
-    rendered = false;
-    cache = {
-      attributes: compiler.attributes,
-      values: compiler.values,
-      template: compiler.template,
-    };
-
-    start.after(compiler.fragment);
-  }
-
-  const oldValues = cache.template.values;
-  const newValues = template.values;
-
-  for (let index = 0; index < newValues.length; index++) {
-    const oldValue = oldValues[index];
-    const newValue = newValues[index];
-
-    if (oldValue === newValue && rendered) {
-      continue;
-    }
-
-    if (index in cache.attributes) {
-      renderAttribute(cache.attributes[index], oldValue, newValue);
-    } else if (index in cache.values) {
-      renderValues(cache.values[index], oldValue, newValue);
-    } else {
-      throw new Error("index does not exist in template");
-    }
-  }
-
-  cache.template = template;
-  caches.set(start, cache);
-}
-
 function render(target: Element, template: Template): void {
-  if (!rendered.has(target)) {
-    while (target.firstChild) {
-      target.firstChild.remove();
-    }
-
-    target.append(new Comment(), new Comment());
-    rendered.add(target);
+  if (!templates.has(target)) {
+    clearElement(target);
+    target.append(new Comment("original"), new Comment("original"));
   }
 
   const start = target.firstChild;
   const end = target.lastChild;
 
   if (start instanceof Comment && end instanceof Comment) {
-    renderTemplate(start, end, template);
+    renderTemplate(start, end, templates.get(target), template);
   } else {
-    throw new Error("start and end markers are missing");
+    throw new Error("start or end markers missing");
   }
+
+  templates.set(target, template);
 }
